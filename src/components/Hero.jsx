c139a81537346d6da80ffa104766e307f1cfee68
Aman Rahees <amanrahees80@gmail.com>
2024-03-08 20:57:14 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <section
+      id="Home"
+      className="flex xl:flex-row flex-col justify-center gap-10 max-container min-h-screen"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28">
+        <p className="">Our Summer Collection</p>
+        <h1 className="">
+          <span>New Arrival</span>
+          <br />
+          <span>Nike</span> Shoes
+        </h1>
+        <p className="">disc</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
